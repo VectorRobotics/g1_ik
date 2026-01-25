73642e5f7f02a1966fb18279c720038f33c76b37
Kushal Agarwal <kushalagarwal444@gmail.com>
2026-01-25 10:23:33 -0500

    ros integration

--- launch/display.launch.py
+++ launch/display.launch.py
@@ -2,34 +2,63 @@ from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.parameter_descriptions import ParameterValue
+from launch.substitutions import Command
+
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
     package_path = FindPackageShare('g1_ik')
-    default_model_path = PathJoinSubstitution(['assets', 'g1', 'g1_29dof_with_hand_rev_1_0.urdf'])
+    default_model_path = PathJoinSubstitution([package_path, 'assets', 'g1', 'g1_29dof_with_hand_rev_1_0_ros.urdf'])
     default_rviz_config_path = PathJoinSubstitution([package_path, 'rviz', 'model.rviz'])
 
     # These parameters are maintained for backwards compatibility
-    gui_arg = DeclareLaunchArgument(name='gui', default_value='true', choices=['true', 'false'],
+    gui_arg = DeclareLaunchArgument(name='jsp_gui', default_value='false', choices=['true', 'false'],
                                     description='Flag to enable joint_state_publisher_gui')
     ld.add_action(gui_arg)
-    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+    rviz_arg = DeclareLaunchArgument(name='rviz_config', default_value=default_rviz_config_path,
                                      description='Absolute path to rviz config file')
     ld.add_action(rviz_arg)
 
-    # This parameter has changed its meaning slightly from previous versions
-    ld.add_action(DeclareLaunchArgument(name='model', default_value=default_model_path,
-                                        description='Path to robot urdf file relative to g1_ik package'))
+    robot_description_content = ParameterValue(Command(['xacro ', default_model_path]), value_type=str)
 
-    ld.add_action(IncludeLaunchDescription(
-        PathJoinSubstitution([FindPackageShare('urdf_launch'), 'launch', 'display.launch.py']),
-        launch_arguments={
-            'urdf_package': 'g1_ik',
-            'urdf_package_path': LaunchConfiguration('model'),
-            'rviz_config': LaunchConfiguration('rvizconfig'),
-            'jsp_gui': LaunchConfiguration('gui')}.items()
+    robot_state_publisher_node = Node(package='robot_state_publisher',
+                                      executable='robot_state_publisher',
+                                      parameters=[{
+                                          'robot_description': robot_description_content,
+                                      }])
+
+    ld.add_action(robot_state_publisher_node)
+
+    ld.add_action(Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        condition=UnlessCondition(LaunchConfiguration('jsp_gui')),
+        parameters=[{
+            'source_list': ['ik/joint_states'],
+        }]
+    ))
+    ld.add_action(Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        condition=IfCondition(LaunchConfiguration('jsp_gui'))
     ))
 
+    ld.add_action(Node(
+        package='rviz2',
+        executable='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rviz_config')],
+    ))
+
+    ld.add_action(Node(
+        package='g1_ik',
+        executable='ik_joint_state_publisher',
+        name='ik_joint_state_publisher',
+        output='screen',
+    ))
     return ld
